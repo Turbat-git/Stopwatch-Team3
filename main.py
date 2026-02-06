29056ce8b97e4a283c29cd2a25643f247d697845
HeyWray <124367445+HeyWray@users.noreply.github.com>
2026-02-06 13:46:02 +0800

    hours, min, second

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
 import asyncio
 import time
 
-# stop_time = 0.0
+
 start_time = time.time()
+hour_time = 0
+minute_time = 0
+seconds_time = 0
+micro_seconds = 0.0
 
 
 async def timer():
-    # stop_time += 0.1
+    global micro_seconds
+    global seconds_time
+    global minute_time
+    global hour_time
+
+    micro_seconds += 0.1
     await asyncio.sleep(.1)
-    print(f"{round(time.time() - start_time)}")
+
+    # count up to next time interval
+    if micro_seconds >= 1:
+        seconds_time += 1
+        micro_seconds -= 1
+    if seconds_time >= 60:
+        minute_time += 1
+        seconds_time -= 60
+    if minute_time >= 60:
+        hour_time += 1
+        minute_time -= 60
+
+    print(f"{hour_time}:{minute_time}:{seconds_time}")
+
 
 while True:
     asyncio.run(timer())
